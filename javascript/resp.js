37ec1e65ad7f88951768f1ebd08d86df460fdbec
Harmeet Singh <96486082+Harmeet135@users.noreply.github.com>
2022-04-24 20:10:30 +0530

    Update resp.js

--- javascript/resp.js
+++ javascript/resp.js
@@ -9,3 +9,32 @@ burger.addEventListener('click',()=>{
     navbarlist.classList.toggle('vclass');
     navbar.classList.toggle('hnav');
 })
+
+
+const appearOptions = {
+    rootMargin: "0px 0px -200px 0px"
+
+};
+
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
